74926096651aa1f72121322f2d96934edbcb57d3
Dagadu <DagaduJ@stanbic.com.gh>
2018-05-01 23:47:36 +0000

    test

--- index.js
+++ index.js
@@ -21,7 +21,69 @@ let fxd = req.body.queryResult.parameters['fxd']; // city is a required param
 let vxd = req.body.queryResult.parameters['vxd'];
 let amount = req.body.queryResult.parameters['amount'];
 
+callCurrencyApi(fxd, vxd, amount).then((output) => {
 
+  res.json(
+  { 
+"fulfillmentText": output,
+"fulfillmentMessages": [
+  {
+    "card": {
+      "title": "card title",
+      "subtitle": "card text",
+      "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+      "buttons": [
+        {
+          "text": "button text",
+          "postback": "https://assistant.google.com/"
+        }
+      ]
+    }
+  }
+],
+"source": "example.com",
+"payload": {
+  "google": {
+    "expectUserResponse": true,
+    "richResponse": {
+      "items": [
+        {
+          "simpleResponse": {
+            "textToSpeech": "this is a simple response"
+          }
+        }
+      ]
+    }
+  },
+  "facebook": {
+    "text": "Hello, Facebook!"
+  },
+  "slack": {
+    "text": "This is a text response for Slack."
+  }
+},
+"outputContexts": [
+  {
+    "name": "projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name",
+    "lifespanCount": 5,
+    "parameters": {
+      "param": "param value"
+    }
+  }
+],
+"followupEventInput": {
+  "name": "event name",
+  "languageCode": "en-US",
+  "parameters": {
+    "param": "param value"
+  }
+}}
+
+  ); // Return the results of the weather API to Dialogflow
+  }).catch(() => {
+    res.json({ 'fulfillmentText': `I don't know how to convert this. Maybe I need to go back to Class 1.` });
+  });
+};
 
 
 return res.json({
@@ -98,72 +160,10 @@ exports.dialogflowWeatherWebhook = (req, res) => {
 }
 ///////////////////////////////////////
 
-
+*/
 
    // Call the weather API
-  /*callCurrencyApi(fxd, vxd, amount).then((output) => {
 
-  res.json(
-  { 
-"fulfillmentText": output,
-"fulfillmentMessages": [
-  {
-    "card": {
-      "title": "card title",
-      "subtitle": "card text",
-      "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
-      "buttons": [
-        {
-          "text": "button text",
-          "postback": "https://assistant.google.com/"
-        }
-      ]
-    }
-  }
-],
-"source": "example.com",
-"payload": {
-  "google": {
-    "expectUserResponse": true,
-    "richResponse": {
-      "items": [
-        {
-          "simpleResponse": {
-            "textToSpeech": "this is a simple response"
-          }
-        }
-      ]
-    }
-  },
-  "facebook": {
-    "text": "Hello, Facebook!"
-  },
-  "slack": {
-    "text": "This is a text response for Slack."
-  }
-},
-"outputContexts": [
-  {
-    "name": "projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name",
-    "lifespanCount": 5,
-    "parameters": {
-      "param": "param value"
-    }
-  }
-],
-"followupEventInput": {
-  "name": "event name",
-  "languageCode": "en-US",
-  "parameters": {
-    "param": "param value"
-  }
-}}
-
-  ); // Return the results of the weather API to Dialogflow
-  }).catch(() => {
-    res.json({ 'fulfillmentText': `I don't know how to convert this. Maybe I need to go back to Class 1.` });
-  });
-};
 
 function callCurrencyApi (fxd, vxd, amount) {
   return new Promise((resolve, reject) => {
@@ -201,4 +201,4 @@ function callCurrencyApi (fxd, vxd, amount) {
       });
     });
   });
-}*/
+}
