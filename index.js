4708fbea4afc6dc3e9fd04ca9662d5f18a3611fa
Dagadu <DagaduJ@stanbic.com.gh>
2018-05-01 22:59:07 +0000

    ts

--- index.js
+++ index.js
@@ -4,16 +4,12 @@ var listeningport = process.env.PORT || 6000;
 
 const express = require('express');
 const bodyparser = require('body-parser');
-
 const app= express();
-
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: true}));
-
 app.listen(listeningport, function () {
 console.log('Get User Details BOT Running at ...' + listeningport);
 });
-
 app.post('/rateconvertor', (req,res) => {
 
 console.log(' GetUserdetails being called by dialogflow...');
