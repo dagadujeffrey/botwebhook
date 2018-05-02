e3d54b36e7738cf4973e0aa29e530018cd587f99
Dagadu <DagaduJ@stanbic.com.gh>
2018-05-02 00:17:47 +0000

    te

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const http = require('http');
 const host = 'stanghbot.herokuapp.com';
 const listeningport = process.env.PORT || 6000;
-
+const webhookresponse = '';
 const express = require('express');
 const bodyparser = require('body-parser');
 
@@ -21,115 +21,53 @@ let fxd = req.body.queryResult.parameters['fxd']; // city is a required param
 let vxd = req.body.queryResult.parameters['vxd'];
 let amount = req.body.queryResult.parameters['amount'];
 
-let converted_amount = callCurrencyApi(fxd, vxd, amount);
-let output = converted_amount;
-  
+callCurrencyApi(fxd, vxd, amount).then((output) => {
+
+   webhookresponse = output;
+
+});  
 return res.json({
       
-      fulfillmentText: output,
+      fulfillmentText: webhookresponse,
        source: "From our Webservice"
      });
 });
 
-/*
-/////////////////////////////
-exports.dialogflowWeatherWebhook = (req, res) => {
-  // Get the fxd and vxd and amount from the request
-  let fxd = req.body.queryResult.parameters['fxd']; // city is a required param
-  let vxd = req.body.queryResult.parameters['vxd'];
-  let amount = req.body.queryResult.parameters['amount'];
-  res.json(
-  { 
-"fulfillmentText": "Test",
-"fulfillmentMessages": [
-  {
-    "card": {
-      "title": "card title",
-      "subtitle": "card text",
-      "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
-      "buttons": [
-        {
-          "text": "button text",
-          "postback": "https://assistant.google.com/"
-        }
-      ]
-    }
-  }
-],
-"source": "example.com",
-"payload": {
-  "google": {
-    "expectUserResponse": true,
-    "richResponse": {
-      "items": [
-        {
-          "simpleResponse": {
-            "textToSpeech": "this is a simple response"
-          }
-        }
-      ]
-    }
-  },
-  "facebook": {
-    "text": "Hello, Facebook!"
-  },
-  "slack": {
-    "text": "This is a text response for Slack."
-  }
-},
-"outputContexts": [
-  {
-    "name": "projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name",
-    "lifespanCount": 5,
-    "parameters": {
-      "param": "param value"
-    }
-  }
-],
-"followupEventInput": {
-  "name": "event name",
-  "languageCode": "en-US",
-  "parameters": {
-    "param": "param value"
-  }
-}}
 
-  );
-}
-///////////////////////////////////////
-
-*/
-
-   // Call the weather API
-
-
-function callCurrencyApi (fxd, vxd, amount) 
-{
-  
+function callCurrencyApi (fxd, vxd, amount) {
+  return new Promise((resolve, reject) => {
+    // Create the path for the HTTP request to get the weather
     let path = '/api/convertor/' + fxd + '/' + vxd + '/' + amount;
     console.log('API Request: ' + host + path);
 
     // Make the HTTP request to get the weather
     http.get({host: host, path: path}, (res) => {
-    let body = ''; // var to store the response chunks
-    res.on('data', (d) => { body += d; }); // store each response chunk
-    res.on('end', () => {
+      let body = ''; // var to store the response chunks
+      res.on('data', (d) => { body += d; }); // store each response chunk
+      res.on('end', () => {
         // After all the data has been received parse the JSON for desired data
-    let response = JSON.parse(body);
-    let output = '';
-    if (!response)
-    {
-        output = `ERR`;
-    }
-    else
-    {
+        let response = JSON.parse(body);
+        let output = '';
+        if (!response)
+        {
+          output = `One or more unsupported currencies. Please try again with Euros, Dollars, Ghana Cedis, Pounds or Rands`;
+        }
+        else
+        {
         let rate = response['rate'];
         console.log('Rate : ' + rate);
         let converted_amount = amount * rate;
         // Create response
         output = `${amount} ${fxd} to ${vxd} at today's rate is ${converted_amount}`;
-    }
-  }
-       
-}
+      }
+        // Resolve the promise with the output text
+        //console.log(output);
+        resolve(output);
+      });
+      res.on('error', (error) => {
+        console.log(`Error calling the weather API: ${error}`)
+        reject();
+      });
+    });
+  });
 }
